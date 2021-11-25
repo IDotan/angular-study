d7e97c10fd575002872baf80a39847501f3448ab
IDotan <itai145@gmail.com>
2021-11-25 08:16:25 +0200

    use innerHTML to footer icon

--- class-app/src/app/footer/footer.component.ts
+++ class-app/src/app/footer/footer.component.ts
@@ -13,8 +13,8 @@ export class FooterComponent implements OnInit {
   ngOnInit(): void {
   }
   SOCIALS: Social_buttons[] = [
-    { name: 'linkedin', URL: 'https://www.linkedin.com/in/idotan/', icon: 'fab fa-linkedin' },
-    { name: 'facebook', URL: 'https://www.facebook.com/itai.dotan.73/', icon: 'fab fa-facebook-square' }
+    { name: 'linkedin', URL: 'https://www.linkedin.com/in/idotan/', icon: "<i class='fab fa-linkedin'></i>" },
+    { name: 'facebook', URL: 'https://www.facebook.com/itai.dotan.73/', icon: "<i class='fab fa-facebook-square'></i>" }
   ];
 
   click_link(url: Social_buttons) { window.open(url.URL, '_blank') }
